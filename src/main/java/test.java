45ffd6ceb4bae0eb05f248ad153be3b43d186fb8
kahuk <kahuk@MSI>
2024-03-18 19:32:30 +0700

    tri ngu

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -29,6 +29,7 @@ public class test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("tri ngu");
 		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
